86c40d3bb3379b20a1934fa9a0d9d5a94d8e77ed
guima <gabrielguimasr@gmail.com>
2024-09-29 18:17:53 -0300

    Grafo gerado

--- Filosofos/filosofosutils.py
+++ Filosofos/filosofosutils.py
@@ -1,7 +1,9 @@
 import json
 import gzip
 import networkx as nx
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def json_to_csv(caminho_arquivos='../data/phi.json'):
     with open(caminho_arquivos, 'r') as file:
@@ -23,48 +25,57 @@ def json_to_csv(caminho_arquivos='../data/phi.json'):
     return filosofos
 
 
-def get_grafo(json_data, nos_desejados=None, retritivo=False, parametro='influencer'):
+def get_grafo(csv='../data/phi.csv', nos_desejados=None, retritivo=False, parametro='influencia'):
     grafo_direcidonado = nx.DiGraph()
-    #cria um grafo direcionado
+    
+    filosofos = pd.read_csv(csv)
+    
+    filosofos = filosofos.set_index('influenciado')
     
     if nos_desejados is None:
-        #caso não possua nos desejados, utiliza de todas as conexões
-        nos_desejados = set(json_data.keys())
+        nos_desejados = filosofos.index
     
     for no in nos_desejados:
-        data = json_data[no]
-        #percorre os nos selecionados e suas conexões, desejados ou todos
-        by_set = set(map(extract_id, data[parametro]))
-        #certifica conexões únicas
-        for by in by_set:
-            if retritivo and by not in nos_desejados:
-                continue
-            grafo_direcidonado.add_edge(no, by)
-            #adiciona os vertices
+        if no in filosofos.index: 
+            data = filosofos.loc[no]
+            #percorre os nos selecionados e suas conexões, desejados ou todos
+            by_set = set(data[parametro])
+            #certifica conexões únicas
+            for by in by_set:
+                if retritivo and by not in nos_desejados:
+                    continue
+                grafo_direcidonado.add_edge(no, by)
+                #adiciona os vertices
 
     return grafo_direcidonado
 
-def gera_grafico(grafo,df,numero_disrupcao=0,cor_principais='green',cor_secundarios='gray',tamanho_texto=10,largura=10,altura=10):
-    nos = grafo.nodes()
-    grau = nx.degree(grafo)
-    #pega os nos e os graus
+def gera_grafico(grafo,cor_principais='green',cor_secundarios='gray',tamanho_texto=10,largura=10,altura=10):#para testar se o grafo esta correto
+    nos = np.array(grafo.nodes())
+    #pega os nos
 
-    
-    condicao_disrupcao = df[df.get('disruption') >= numero_disrupcao] if numero_disrupcao >= 0 else df[df.get('disruption') <= numero_disrupcao]
-    #cria df filtrado por mais ou menos disrupção
+    nos_sorteados = np.random.choice(nos,50,replace=False)
+    #escolhendo nos aleatorios
 
-    cor_nos = [cor_principais if no in condicao_disrupcao.index else cor_secundarios for no in nos]
+
+    grafo_amostra = get_grafo(nos_desejados=list(nos_sorteados))
+
+    novo_nos = np.array(grafo_amostra.nodes())
+
+    nos_nomes =  np.random.choice(novo_nos,10,replace=False)
+
+    cor_nos = [cor_principais if no in nos_nomes  else cor_secundarios for no in novo_nos]
     #cria uma lista de cores para cada nó
 
-    nomes_nos = {no: df.loc[no].get('name') if no in condicao_disrupcao.index else '' for no in nos}
-    #cria um dicionario para cada nó
+    nomes_nos ={no: no if no in nos_nomes else '' for no in novo_nos}
+    #cria um dicionario para cada nó sorteado em 10
 
+    grau = nx.degree(grafo_amostra)
 
     ax = plt.subplots(figsize=(largura, altura))
 
     return nx.draw_networkx(
-        grafo,
-        pos=nx.kamada_kawai_layout(grafo),
+        grafo_amostra,
+        pos=nx.kamada_kawai_layout(grafo_amostra),
         with_labels=True,
         node_size=[v * 100 for _, v in grau],#tamanho do nó de acordo com seu grau
         node_color=cor_nos,
