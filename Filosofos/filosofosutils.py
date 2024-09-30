27ef0c95990c6fe2c0d4364dfe25ab4198b5776a
guima <gabrielguimasr@gmail.com>
2024-09-30 20:58:18 -0300

    Novo Dataset e Grafo Configurado Co-authored-by: mateusacq <mateus@andinossi.com.br>

--- Filosofos/filosofosutils.py
+++ Filosofos/filosofosutils.py
@@ -5,51 +5,54 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def json_to_csv(caminho_arquivos='../data/phi.json'):
-    with open(caminho_arquivos, 'r') as file:
+def get_pandas_csv():
+    caminho_arquivos='../data/phi_p_i_ids.json'
+    
+    with open(caminho_arquivos, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
-    filosofos = pd.DataFrame()
+    pandas = pd.DataFrame(columns=['id','filosofo'])
+    
+    array_id = []
 
-    filosofos['influenciado'] = "" 
-    filosofos['influencia'] = "" 
-
-    for filosofo in data['results']['bindings']:
-
-        nova_linha = {'influenciado': filosofo['p']['value'].split('/')[4], 'influencia' : filosofo['influenced']['value'].split('/')[4]}
-
-        filosofos = filosofos._append(nova_linha, ignore_index = True)
+    for phi in data['results']['bindings']:
         
-    filosofos.to_csv('../data/phi.csv',index=False)
+        if phi['p']['id'] not in array_id:
+            nova_linha = {'id': phi['p']['id'] , 'filosofo' : phi['p']['value']}
+            array_id.append(phi['p']['id'])
+            pandas = pandas._append(nova_linha, ignore_index = True)
+        
+        if phi['influenced']['id'] not in array_id:
+            nova_linha = {'id': phi['influenced']['id'] , 'filosofo' : phi['influenced']['value']}
+            array_id.append(phi['influenced']['id'])
+            pandas = pandas._append(nova_linha, ignore_index = True)
 
-    return filosofos
+    pandas = pandas.sort_values('id').set_index('id')
+    pandas.to_csv('../data/phi.csv',index=False)
+
+    return pandas
 
 
-def get_grafo(csv='../data/phi.csv', nos_desejados=None, retritivo=False, parametro='influencia'):
+def get_grafo(nos_desejados=[], restritivo=False, parametro='influencia'):
+    
+    caminho_arquivos='../data/phi_p_i_ids.json'
+    
+    with open(caminho_arquivos, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
     grafo_direcidonado = nx.DiGraph()
-    
-    filosofos = pd.read_csv(csv)
-    
-    filosofos = filosofos.set_index('influenciado')
-    
-    if nos_desejados is None:
-        nos_desejados = filosofos.index
-    
-    for no in nos_desejados:
-        if no in filosofos.index: 
-            data = filosofos.loc[no]
-            #percorre os nos selecionados e suas conexões, desejados ou todos
-            by_set = set(data[parametro])
-            #certifica conexões únicas
-            for by in by_set:
-                if retritivo and by not in nos_desejados:
-                    continue
-                grafo_direcidonado.add_edge(no, by)
-                #adiciona os vertices
+
+    for phi in data['results']['bindings']:
+        if len(nos_desejados)==0 : 
+            grafo_direcidonado.add_edge(phi['p']['id'],phi['influenced']['id'])
+        elif restritivo and phi['influenced']['id'] not in nos_desejados:
+            continue
+        elif phi['p']['id'] in nos_desejados:
+            grafo_direcidonado.add_edge(phi['p']['id'],phi['influenced']['id']) 
 
     return grafo_direcidonado
 
-def gera_grafico(grafo,cor_principais='green',cor_secundarios='gray',tamanho_texto=10,largura=10,altura=10):#para testar se o grafo esta correto
+def gera_grafico(grafo,df,cor_principais='green',cor_secundarios='gray',tamanho_texto=10,largura=10,altura=10):#para testar se o grafo esta correto
     nos = np.array(grafo.nodes())
     #pega os nos
 
@@ -62,12 +65,12 @@ def gera_grafico(grafo,cor_principais='green',cor_secundarios='gray',tamanho_tex
     novo_nos = np.array(grafo_amostra.nodes())
 
     nos_nomes =  np.random.choice(novo_nos,10,replace=False)
+    
+    nomes_nos ={no: df.loc[no].get('filosofo') if no in nos_nomes else '' for no in novo_nos}
+    #cria um dicionario para cada nó sorteado em 10
 
     cor_nos = [cor_principais if no in nos_nomes  else cor_secundarios for no in novo_nos]
     #cria uma lista de cores para cada nó
-
-    nomes_nos ={no: no if no in nos_nomes else '' for no in novo_nos}
-    #cria um dicionario para cada nó sorteado em 10
 
     grau = nx.degree(grafo_amostra)
 
@@ -80,6 +83,6 @@ def gera_grafico(grafo,cor_principais='green',cor_secundarios='gray',tamanho_tex
         node_size=[v * 100 for _, v in grau],#tamanho do nó de acordo com seu grau
         node_color=cor_nos,
         edge_color='lightgray',
-        labels=nomes_nos,
+        labels= nomes_nos,
         font_weight=tamanho_texto,
     )
